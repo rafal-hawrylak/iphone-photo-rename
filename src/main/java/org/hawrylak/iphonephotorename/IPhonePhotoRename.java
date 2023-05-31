2e88ba2a8e9b9b392cc99d57fadc4b5d8e588177
Rafal <rafal.hawrylak@gmail.com>
2023-05-31 14:44:35 +0200

    git ignore apply

--- src/main/java/org/hawrylak/iphonephotorename/IPhonePhotoRename.java
+++ src/main/java/org/hawrylak/iphonephotorename/IPhonePhotoRename.java
@@ -4,8 +4,11 @@ import com.drew.imaging.ImageMetadataReader;
 import com.drew.imaging.ImageProcessingException;
 import com.drew.metadata.Directory;
 import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
+import com.drew.metadata.exif.ExifSubIFDDirectory;
 import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -19,10 +22,30 @@ public class IPhonePhotoRename {
 
     private void run(String sampleFile) {
         logger.info("IPhone Photo Rename (reads data from EXIF of jpeg");
+        var date = readDateFromFile(sampleFile);
+        logger.info(sampleFile + " -> " + date);
+    }
+
+    private Date readDateFromFile(String fileName) {
+        try {
+            Metadata metadata = ImageMetadataReader.readMetadata(new File(fileName));
+            ExifSubIFDDirectory directory = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
+            return directory.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
+        } catch (ImageProcessingException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void printAllMetadataFromFile(String sampleFile) {
         try {
             Metadata metadata = ImageMetadataReader.readMetadata(new File(sampleFile));
             for (Directory directory : metadata.getDirectories()) {
-                logger.info(directory.getName());
+                logger.info("directory: " + directory.getName());
+                for (Tag tag : directory.getTags()) {
+                    logger.info("tag: " + tag);
+                }
             }
         } catch (ImageProcessingException e) {
             throw new RuntimeException(e);
